8dd0ef915b0ecaf38f328d5f8be0aebad84adba2
randomizerkatt <ABroderick@student.neumont.edu>
2020-08-06 13:35:37 -0600

    Added board numbers

--- chess_packages/chess_package/Board_Class.java
+++ chess_packages/chess_package/Board_Class.java
@@ -197,10 +197,11 @@ public class Board_Class { // Board class
 	}
 
 	public void show_board() { // show current board function
+		System.out.println("  0  1  2  3  4  5  6  7");
 
 		for (int i = 0; i < 8; i++) {
+			System.out.print(i + " ");
 			for (int j = 0; j < 8; j++) {
-
 				if (array_2d_piece[i][j] != null) {
 					System.out.printf(array_2d_piece[i][j].symbol + "  ");
 				} else {
---
+++ tests/tests/Chess960Tests.java
@@ -0,0 +1,18 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+import chess_package.Board_Class;
+
+class Chess960Tests {
+	
+	Board_Class board = new Board_Class();
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
